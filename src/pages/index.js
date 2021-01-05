8d51febad3979df177196eb44848d1604607b00e
Marcus Schreiter <marcus.schreiter@meso.design>
2021-01-05 11:57:50 +0100

    update data fetching, add fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,19 @@
-export default function Index({ data }) {
+import api from "../lib/api";
+
+export default function Index({ pagesData, links }) {
   return <div>hello world</div>;
+}
+
+export async function getStaticProps() {
+  const data = await api.fetchJson("/api/content");
+  const pagesData = data.data.pages;
+  const links = data.data.links;
+
+  return {
+    props: {
+      pagesData,
+      links,
+    },
+    revalidate: 2,
+  };
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,6 @@ module.exports = {
       colors: {
         black: "var(--color-black)",
         alpha: "var(--color-alpha)",
-        alphaTransparent: "var(--color-alpha-transparent)",
-        beta: "var(--color-beta)",
-        gamma: "var(--color-gamma)",
-        delta: "var(--color-delta)",
-        deltaLight: "var(--color-delta-light)",
-        epsilon: "var(--color-epsilon)",
-        epsilonLight: "var(--color-epsilon-light)",
       },
       fontSize: {
         tiny: "0.65rem",
