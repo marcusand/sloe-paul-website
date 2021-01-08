1886a723233a2366e5edafcdc7c5cae9f4d6ec45
Marcus Schreiter <marcus.schreiter@meso.design>
2021-01-08 19:31:30 +0100

    update production cms url and fix build

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 export default function Layout({ pagesData, links, children }) {
+  if (!pagesData) return null;
   return (
     <div className="layout w-full h-full flex flex-col px-1 pb-1 md:pb-0 md:px-4 uppercase">
       <Header pages={pagesData} links={links} />
