25d172b9042acbaa40ee0bd172ed499c0e9bcc65
Marcus Schreiter <marcus.schreiter@meso.design>
2021-01-13 22:13:42 +0100

    fix link with external url

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,11 @@ export default function Navigation({ pages, className, onItemClick }) {
     <div className={`${className} `}>
       {pages.map((page) => (
         <div key={page.id}>
-          <a href={`#${page.slug}`} onClick={() => (onItemClick ? onItemClick() : null)}>
+          <a
+            href={page.external_url ? page.external_url : `#${page.slug}`}
+            target={page.external_url ? "__blank" : ""}
+            onClick={() => (onItemClick ? onItemClick() : null)}
+          >
             <Button>{page.title}</Button>
           </a>
         </div>
