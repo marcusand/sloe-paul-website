26e1b958e74f39ddde047aa6511259ea8ebe408d
Marcus Schreiter <marcus.schreiter@meso.design>
2021-01-17 22:49:00 +0100

    add birds animations to header

---
+++ src/components/Bird.js
@@ -0,0 +1,47 @@
+import { useEffect, useRef } from "react";
+
+export default function Bird({ width, fromLeft, speed, index, y, onAnimationEnded }) {
+  const container = useRef();
+  const start = useRef();
+  const offset = fromLeft ? -200 : 200;
+
+  const animate = (timestamp) => {
+    if (!container.current) return;
+
+    const windowWidth = window.innerWidth;
+
+    if (!start.current) {
+      start.current = timestamp;
+    }
+
+    const elapsed = timestamp - start.current;
+
+    const move = fromLeft ? speed * elapsed + offset : -(speed * elapsed - offset);
+
+    container.current.style.transform = `translate(${move}px, ${y}px)`;
+
+    if (Math.abs(move) < windowWidth) {
+      window.requestAnimationFrame(animate);
+    } else {
+      onAnimationEnded(index);
+    }
+  };
+
+  useEffect(() => {
+    if (!container.current) return;
+    window.requestAnimationFrame(animate);
+  }, [container]);
+
+  return (
+    <div
+      className={`absolute top-0 ${fromLeft ? "left-0" : "right-0"}`}
+      ref={container}
+      style={{ transform: `translateX(${offset}px)` }}
+    >
+      <img
+        src="animations/bird.png"
+        style={{ width: `${width}px`, transform: `scaleX(${fromLeft ? "-1" : "1"})` }}
+      />
+    </div>
+  );
+}
--- src/components/Container/Hero.js
+++ src/components/Container/Hero.js
@@ -1,9 +1,68 @@
+import { useEffect, useRef, useState } from "react";
+import helpers from "../../lib/helpers";
+import Bird from "../Bird";
+
 export default function Hero({ children, className }) {
+  const [birdsData, setBirdsData] = useState([]);
+  const birdsRef = useRef([]);
+  const containerRef = useRef();
+
+  const randomTimeout = () => Math.random() * 30000;
+
+  const addBird = () => {
+    const birdsCopy = Object.assign([], birdsRef.current);
+    const newBird = {
+      index: Date.now(),
+      width: helpers.getRandomArbitrary(90, 300),
+      fromLeft: Math.random() > 0.5,
+      speed: helpers.getRandomArbitrary(0.1, 0.5),
+      y: Math.round(
+        helpers.getRandomArbitrary(0, containerRef.current.offsetHeight * 0.9),
+      ),
+    };
+
+    birdsCopy.push(newBird);
+    birdsRef.current = birdsCopy;
+    setBirdsData(birdsRef.current);
+    setTimeout(addBird, randomTimeout());
+  };
+
+  const handleAnimationEnded = (index) => {
+    const arrayIndex = birdsRef.current.findIndex((element) => element.index === index);
+    const newData = [
+      ...birdsRef.current.slice(0, arrayIndex),
+      ...birdsRef.current.slice(arrayIndex + 1, birdsRef.current.length + 1),
+    ];
+
+    birdsRef.current = newData;
+  };
+
+  useEffect(() => {
+    addBird();
+    setTimeout(addBird, randomTimeout());
+  }, []);
+
+  useEffect(() => {
+    setBirdsData(birdsRef.current);
+  }, [birdsRef.current]);
+
   return (
     <div
       className={`content-container relative md:h-hero mb-4 p-0 overflow-hidden bg-transparent ${className}`}
+      ref={containerRef}
     >
       {children}
+      {birdsData.map(({ index, width, speed, fromLeft, y }) => (
+        <Bird
+          key={index}
+          index={index}
+          width={width}
+          speed={speed}
+          fromLeft={fromLeft}
+          y={y}
+          onAnimationEnded={handleAnimationEnded}
+        />
+      ))}
     </div>
   );
 }
