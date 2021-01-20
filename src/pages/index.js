8366737715089ec1b51dd8b293546270e53aa325
Marcus Schreiter <marcus.schreiter@meso.design>
2021-01-20 22:15:00 +0100

    add favicon and app icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,16 @@ export default function Index({ pagesData }) {
         <meta name="description" content="Homepage of musician Sloe Paul" />
         <meta name="author" content="Marcus Schreiter" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png" />
+        <link rel="manifest" href="/icon/site.webmanifest" />
+        <link rel="mask-icon" href="/icon/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/icon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#fab1c0" />
+        <meta name="msapplication-config" content="/icon/browserconfig.xml" />
+        <meta name="theme-color" content="#fab1c0" />
       </Head>
       <About data={pagesData[0]} />
       <Releases data={pagesData[1]} />
